4b499b84c83fc04169153108d5238fde6153c997
Coinbene Official <github@coinbene.com>
2020-02-24 16:13:07 +0800

    Update util.py

--- util.py
+++ util.py
@@ -3,8 +3,14 @@ import hashlib
 import hmac
 import json
 import urllib
-
 import requests
+
+import sys
+import os
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
+
 
 
 def request_nosign_get(url, dic):
